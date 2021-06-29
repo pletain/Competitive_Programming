4af9757767675d44d22bdf914451b7413ac10099
김태형 <thk12378905@gmail.com>
2021-06-29 04:27:44 +0000

    10814

---
+++ 10814.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N, age[10001];
+string name[10001];
+
+void quickSort(int i, int j)
+{
+	if(i>=j) return;
+	int pivot = age[(i+j)/2];
+	int left = i;
+	int right = j;
+	
+	while(left<=right)
+	{
+		while(age[left]<pivot) left++;
+		while(age[right]>pivot) right--;
+		if(left<=right)
+		{
+			swap(age[left],age[right]);
+			swap(name[left],name[right]);
+			left++; right--;
+		}
+	}
+	quickSort(i,right);
+	quickSort(left,j);
+}
+
+
+int main(){
+	for(int i=0; i<N; i++){
+		cin >> age[i];
+		cin >> name[i];
+	}
+	
+	quickSort(0,N-1);
+	for(int i=0; i<N; i++){
+		cout << age[i] << " " << name[i] << endl;
+	}
+	 
+}
