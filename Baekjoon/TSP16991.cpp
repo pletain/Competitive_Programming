37be3928ac67e0216b0215ec7219b8444e4fe066
KIM TAE HYEONG <thk12378905@gmail.com>
2021-07-30 18:15:34 +0900

    Update TSP16991.cpp

--- Baekjoon/TSP16991.cpp
+++ Baekjoon/TSP16991.cpp
@@ -1,25 +1,14 @@
 #include<iostream>
-#include<cmath>
+#include<algorithm>
+#include<math.h>
 using namespace std;
 
-struct Point{
-	int x, y;
-};
-
 int n;
-float w[16][16];
-Point xy[16];
-int memo[16][1 << 16];
+int crd[2][16];
+double w[16][16];
+double memo[16][1 << 16];
 
-
-
-double Distance(const Point& p1, const Point& p2){
-	double distance;
-	distance = sqrt(pow(p1.x-p2.x,2) + pow(p1.y-p2.y,2));
-	return distance;
-}
-
-int tsp(int pos, int mask){
+double tsp(int pos, int mask){
 	if(mask == ((1 << n) - 1)){
 		 if (w[pos][0] == 0)
             return 987654321;
@@ -30,7 +19,7 @@ int tsp(int pos, int mask){
 	if(memo[pos][mask] != 0)
 		return memo[pos][mask];
 
-	int ans = 987654321;
+	double ans = 987654321;
 	for(int nxt = 1; nxt < n; nxt++){
 		if(((mask & (1 << nxt))) == 0 && (w[pos][nxt] != 0)){
 				ans = min(ans, w[pos][nxt] + tsp(nxt, (mask | (1 << nxt))));
@@ -42,20 +31,20 @@ int tsp(int pos, int mask){
 void input(){
 	cin >> n;
 	for(int i=0; i<n; i++){
-		cin >> xy[i].x;
-		cin >> xy[i].y;
+		cin >> crd[0][i];
+		cin >> crd[1][i];
 	}
 	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
-			w[i][j] = Distance(xy[i],xy[j]);
+			w[i][j] = sqrt(pow(crd[0][i] - crd[0][j],2) + pow(crd[1][i] - crd[1][j],2));
 		}
 	}
 }
 
 int main(){
 	input();
-	cout << tsp(0,1) << endl;
+	printf("%lf", tsp(0, 1));
 	
 	return 0;
 }
